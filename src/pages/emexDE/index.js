26fb5ff6f4379c6582165a1babb29652e6d82929
LucaVmu <lucavmu@outlook.de>
2026-08-05 22:30:33 +0200

    Improved emexDE's homepage a lot and fixed the global sections class

--- src/pages/emexDE/index.js
+++ src/pages/emexDE/index.js
@@ -4,9 +4,10 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from '@theme/Layout';
 import ThemedImage from '@theme/ThemedImage';
 import Heading from '@theme/Heading';
+import Admonition from '@theme/Admonition';
 import styles from './index.module.css';
 import ImagePopup from '@site/src/components/ImagePopup';
-import IntroText from './_intro.mdx';
+import AdditionalText from './_additionalText.mdx';
 
 export default function Home() {
   return (
@@ -39,7 +40,20 @@ export default function Home() {
         </div>
       </header>
       <main className={clsx('center', styles.main)}>
-        <IntroText />
+        <Heading as="h1">Introduction</Heading>
+        <div className={clsx('sections', styles.divided)}>
+          <ImagePopup src={useBaseUrl('/emexDE/img/mockup.png')} alt="Mockup" className={styles.showcase} />
+          <div>
+              <Heading as="h2">What is it?</Heading>
+              <p>
+                emexDE is an iOS app that empowers developers with a full toolchain they can use while even being offline for iOS development on iPhone. It supports Swift, C, Objective-C, C++ and Objective-C++. It’s a powerful Xcode alternative that made the impossible possible, a fully on-device iOS IDE that doesn’t even need a cloud and can even be used with airplane mode enabled. It supports officially iOS 16 all the way up to the latest iOS version. You can compile and run iOS apps on the go with ease, using the entire iOS 26.4 SDK.
+              </p>
+          </div>
+        </div>
+        <Admonition type="info">
+          Remember emexDE is still in beta, so some issue might arise! Please report issues over at the [Github](https://github.com/emexlab/emexDE/issues/new/choose).
+        </Admonition>
+        <AdditionalText />
       </main>
     </Layout>
   );
