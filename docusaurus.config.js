682e56e5ab5a816ac8181fd2d962d407295cef6e
LucaVmu <lucavmu@outlook.de>
2026-08-05 23:14:14 +0200

    Changed trailingSlash config as it caused issues with nginx

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'emexLabs',
   tagline: 'Unfolding the power of programming',
   favicon: '/favicon.ico', // More favicons at headTags
-  trailingSlash: false,
+  trailingSlash: true,
 
   titleDelimiter: '·',
 
