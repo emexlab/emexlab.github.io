63a1483e8ed926cba62caea8750c0b72e94c0a51
LucaVmu <lucavmu@outlook.de>
2026-08-20 21:48:54 +0200

    Hopefully fixed lastmod date

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
     [
       '@docusaurus/plugin-sitemap',
       {
-        lastmod: new Date(),
+        lastmod: new Date().toISOString().split('T')[0],
       },
     ],
     [
